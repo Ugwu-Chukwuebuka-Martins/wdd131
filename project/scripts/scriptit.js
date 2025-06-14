d12deccccea13b0cc1eb368a75567a9b05340eca
ugwu-chukwuebuka-martins <cugwu@byupathway.edu>
2025-06-14 02:13:25 -0600

    perfect

--- project/scripts/scriptit.js
+++ project/scripts/scriptit.js
@@ -4,3 +4,10 @@ cYear.textContent = date.getFullYear();
 let lModified = document.querySelector("#lastModified");
 const lDate = new Date(2025,5,6);
 lModified.innerText =  lDate;
+
+const ul = document.querySelector('ul');
+const show = document.querySelector('.hide');
+show.addEventListener('click', ()=>{
+ul.classList.toggle('showd');
+show.classList.toggle('show');
+});
