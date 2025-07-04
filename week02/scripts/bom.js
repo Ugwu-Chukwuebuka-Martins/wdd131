4f6ba0c29a01352337d3af83648f909a565bfdb6
ugwu-chukwuebuka-martins <cugwu@byupathway.edu>
2025-07-04 13:13:29 +0100

    perfect

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -6,14 +6,16 @@ if (input.value.trim() !=""){
     input.focus();
 }
 button.addEventListener("click",function(){
+if (input.value !=""){
 const li = document.createElement("li");
 const deleteButton = document.createElement("button");
 li.textContent = input.value;
+input.value = "";
 deleteButton.textContent = "❌";
 list.append(li);
 li.append(deleteButton);
 deleteButton.addEventListener("click",function(){
 list.removeChild(li);
 input.focus();
-})
+})}
 });
